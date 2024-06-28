10213284a89f8a7b6e11812b34573dde2c39d1e5
Пронин Вадим Викторович <proninvv@vogu35.ru>
2024-06-28 21:44:08 +0300

    Опечатка в переменной

--- src/components/book/BookDetails.jsx
+++ src/components/book/BookDetails.jsx
@@ -9,7 +9,7 @@ export const BookDetails = ({book}) => {
         <ul className="details-list">
             {book.language && <li className="list-entry">Язык: {getReadableLanguage(book.language, languageReducer)}</li>}
             {book.pages && <li className="list-entry">Количество страниц: {book.pages}</li>}
-            {book.ISBNNumber && <li className="list-entry">Номер ISBN: {book.ISBNNumber}</li>}
+            {book.isbnnumber && <li className="list-entry">Номер ISBN: {book.isbnnumber}</li>}
             {book.publicationYear && <li className="list-entry">Год издания: {book.publicationYear}</li>}
             {book.publisher && <li className="list-entry">Издательство: {book.publisher}</li>}
             {book.genre && <li className="list-entry">Жанр: {book.genre}</li>}
